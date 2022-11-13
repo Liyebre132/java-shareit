0456bb899108c9214845b2821acc5f8d72ff20bc
Vladimir Chashchin <liyebre@yandex.ru>
2022-11-13 20:13:20 +0300

    fixed

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -22,7 +22,7 @@ public class Item {
     @Column(name = "description")
     private String description;
 
-    @Column(name = "available")
+    @Column(name = "is_available")
     private boolean available;
 
     @Column(name = "owner_id")
