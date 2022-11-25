9dca664b39533e9a573571b6903299cf5565cecb
Vladimir Chashchin <liyebre@yandex.ru>
2022-11-25 20:43:17 +0300

    Update BookingControllerTests.java

--- src/test/java/ru/practicum/shareit/booking_test/BookingControllerTests.java
+++ src/test/java/ru/practicum/shareit/booking_test/BookingControllerTests.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.booking_test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
